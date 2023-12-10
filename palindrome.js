bdabe44091f07cff9595829d84a974b977689fa3
vijay <vijay.fastxpo@gmail.com>
2023-12-10 23:48:44 +0530

    this is palindrome

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+let str="amma"
+var rev=''
+for(let i=str.length-1;i>=0;i--){
+    rev=rev+str[i]
+}
+
+console.log
+if(rev==str){
+    console.log(" it  is a palindrome",rev,str + " ni enga amma ")
+}
+else{
+    console.log("it is not a palindrome",rev,str)
+}
